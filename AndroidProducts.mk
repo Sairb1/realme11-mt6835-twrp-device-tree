643d23002db132bd50d160766c1855fa59b673ca
Sairb1 <sairbgamer@gmail.com>
2026-05-05 22:41:53 +0530

    device-tree-by-suchit

---
+++ AndroidProducts.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_RE5C34.mk
+
+COMMON_LUNCH_CHOICES := twrp_RE5C34-eng
